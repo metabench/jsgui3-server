7e09329d4dd13baba1d10d91c855dbf890b795ff
metabench <james@metabench.com>
2022-12-11 20:56:07 +0000

    0.0.94 module.js bug fix

--- module.js
+++ module.js
@@ -9,13 +9,13 @@ jsgui.Server_Page_Context = Server_Page_Context;
 // Login = require('../resource/login'),
 //var Server = {};
 
-const Resource_Publisher = require('./publishing/resource-publisher');
+const Resource_Publisher = require('./publishing/http-resource-publisher');
 jsgui.Resource_Publisher = Resource_Publisher;
 jsgui.Server = require('./server');
 jsgui.fs2 = require('./fs2');
 //jsgui.Resource = Resource;
 //console.log('pre scs');
-jsgui.Single_Control_Server = require('./single-control-server');
+//jsgui.Single_Control_Server = require('./single-control-server');
 //console.log('3)jsgui', jsgui);
 
 // Make the Resource_Publisher available?
